df09a98cc3b5d7df9306d40b4a1e0170bb3e6ab4
LM <ulgotbulgot@gmail.com>
2013-10-20 20:09:03 +0200

    new cputests.c, new shell file for cpu tests, add LM in README, extended Make

--- runcputests.sh
+++ runcputests.sh
@@ -5,12 +5,11 @@ test -d $TDIR || mkdir -p $TDIR
 
 force=0.1
 eta=$force
-for paths in 4 8 16 32 64 128 256 512 1024 2048 4096 8162
+paths=8192
+periods=1000000
+GSL_RNG_SEED=$RANDOM ./cputests --force=$force --paths=$paths --periods=$periods > ${TDIR}/gauss_paths${paths}.dat
+for lam in 4 16 64 512
 do
-  GSL_RNG_SEED=$RANDOM ./cputests --force=$force --paths=$paths --periods=1000000 > ${TDIR}/gauss_paths${paths}.dat
-  for lam in 4 16 64 512
-  do
-    Dp=$(echo ${eta}*${eta}/${lam}|bc -l)
-    GSL_RNG_SEED=$RANDOM ./cputests --Dp=$Dp --paths=$paths --periods=1000000 --lambda=$lam > ${TDIR}/poisson_lambda${lam}_paths${paths}.dat
-  done
+  Dp=$(echo ${eta}*${eta}/${lam}|bc -l)
+  GSL_RNG_SEED=$RANDOM ./cputests --Dp=$Dp --paths=$paths --periods=$periods --lambda=$lam > ${TDIR}/poisson_lambda${lam}_paths${paths}.dat
 done
